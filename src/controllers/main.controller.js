477132168ce647a0bfeb6ff99422b51957dd4d25
Lee Chirinos <leemarilychirinos@gmail.com>
2025-07-31 08:29:24 -0300

    se crean vistas

--- src/controllers/main.controller.js
+++ src/controllers/main.controller.js
@@ -1,5 +1,8 @@
+const path = require("path");
+
 const index = (req, res) => {
-  res.send("Hola Mundo!!!");
+  //console.log(__dirname)
+   res.sendFile(path.resolve(__dirname, "../../private/index.html"))
 };
 
 module.exports = {
--- src/controllers/productos.controller.js
+++ src/controllers/productos.controller.js
@@ -3,10 +3,11 @@ const querystring = require("querystring");
 const index = (req, res) => {
   const query = querystring.stringify(req.query);
 
-  fetch("https://fakestoreapi.com/products?" + query)
+ fetch("https://fakestoreapi.com/products?" + query)
     .then((res) => res.json())
-    .then((productos) => res.json(productos));
+    .then((productos) => res.render("productos", { productos }));
 };
+
 
 const show = (req, res) => {
   fetch("https://fakestoreapi.com/products/" + req.params.id)
