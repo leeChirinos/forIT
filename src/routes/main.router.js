ca1ab5b963f4b8ac17678cde826ea2fc78ad847b
Lee Chirinos <leemarilychirinos@gmail.com>
2025-07-30 23:55:55 -0300

    Controladores

--- src/routes/main.router.js
+++ src/routes/main.router.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hola Lee Prueba!!");
-});
+const controller = require("../controllers/main.controller");
+
+router.get("/", controller.index);
 
 module.exports = router;
--- src/routes/productos.router.js
+++ src/routes/productos.router.js
@@ -1,20 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const querystring = require("querystring");
+const controller = require("../controllers/productos.controller");
 
-router.get("/", (req, res) => {
-  const query = querystring.stringify(req.query);
-
-  fetch("https://fakestoreapi.com/products?" + query)
-    .then((res) => res.json())
-    .then((productos) => res.json(productos));
-});
-
-router.get("/:id", (req, res) => {
-  fetch("https://fakestoreapi.com/products/" + req.params.id)
-    .then((res) => res.json())
-    .then((producto) => res.json(producto));
-});
+router.get("/", controller.index);
+router.get("/:id", controller.show);
 
 module.exports = router;
